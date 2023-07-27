adb1e6c5688412fa7a92baef1ede07c936ae0c37
Robert Montgomery <robm4958@gmail.com>
2023-07-27 21:00:16 +0000

    update boot variables

--- hosts/quetzalcoatl/hardware-configuration.nix
+++ hosts/quetzalcoatl/hardware-configuration.nix
@@ -8,6 +8,8 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
   boot.initrd.availableKernelModules = [ "xhci_pci" "thunderbolt" "ahci" "nvme" "usbhid" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-intel" ];
