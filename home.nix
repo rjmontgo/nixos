8d6380c1cf2ff72b94fec693e90eb652e919cbad
Robert Montgomery <robm4958@gmail.com>
2023-12-01 22:22:47 -0500

    Update home.nix with mailctl

--- home.nix
+++ home.nix
@@ -6,11 +6,25 @@
   };
 
   home.packages = with pkgs; [
+    notmuch
+    offlineimap
+    mailctl
+
+    ## prog
     docker
 
+    # typescript
+    nodePackages.typescript-language-server
+    nodePackages.typescript
+    nodejs_21
+
+    # java
     jdk21
     leiningen
+    clojure-lsp
     maven
+
+
   ];
 
   programs.home-manager.enable = true;
